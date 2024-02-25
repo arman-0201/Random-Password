d19bc7db1624664d852bffc2a8aaad95c8fb098a
Arman <145873155+arman-0201@users.noreply.github.com>
2024-02-25 16:53:39 +0400

    Update pass_rand.py

--- pass_rand.py
+++ pass_rand.py
@@ -4,5 +4,6 @@ import string
 def generatepassword(length):
     return ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(length))
 
+print("Chose password length")
 password_length = int(input())
 print("Generated Password:", generatepassword(password_length))
